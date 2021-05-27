c3478ca2618c95e3bb4b546fa132734e5e5e9ba0
user5@iiht.edu-at-774602875240 <pariwesh06>
2021-05-27 10:40:44 +0530

    added links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
-const routes: Routes = [{path:'userform', component:AppComponent}];
+const routes: Routes = [{path:'userform', component:AppComponent},
+{path:'', component:CounterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
